5ac11d67d118bfc78582f8a8018d708da35995cd
Jeffery Sumner <jefferysumner2@gmail.com>
2022-10-14 10:41:04 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿namespace InTheDeep
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerHp = 40;
+            int enemyHp = 20;
+
+            int playerAttack = 5;
+            int enemyAttack = 7;
+
+            int healAmount = 5;
+
+            Random random = new Random();
+
+            while(playerHp > 0 && enemyHp > 0)
+            {
+                // Player Turn
+                Console.WriteLine("--Player Turn--");
+                Console.WriteLine("Player Hp - " + playerHp + ". Enemy Hp - " + enemyHp);
+                Console.WriteLine("Enter 'a' to attack or 'h' to heal");
+
+                string choice = Console.ReadLine();
+
+                if(choice == "a")
+                {
+                    enemyHp -= playerAttack;
+                    Console.WriteLine("You attack your enemy and deal " + playerAttack + " damage");
+                }
+                else
+                {
+                    playerHp += healAmount;
+                    Console.WriteLine("You heal " + healAmount + " health points!");
+                }
+
+                //Enemy Turn
+                if(enemyHp > 0)
+                {
+                    Console.WriteLine("--Enemy Turn--");
+                    Console.WriteLine("Player Hp - " + playerHp + ". Enemy Hp - " + enemyHp);
+                    int enemyChoice = random.Next(0, 2);
+
+                    if(enemyChoice == 0)
+                    {
+                        playerHp -= enemyAttack;
+                        Console.WriteLine("Enemy attacks and deals " + enemyAttack + " damage!");
+                    }
+                    else
+                    {
+                        enemyHp += healAmount;
+                        Console.WriteLine("Enemy restores " + healAmount + " health points!");
+                    }
+                }
+            }
+
+            if(playerHp > 0)
+            {
+                Console.WriteLine("Congratulations, you've defeated the enemy!");
+            }
+            else
+            {
+                Console.WriteLine("The enemy roars in victory and begins to feast on your flesh");
+            }
+        }
+    }
+}
